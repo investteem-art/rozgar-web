68b209586ad196210db441292727574f008c98a4
investteem-art <investteem@gmail.com>
2026-02-16 09:23:26 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,49 @@
+const CACHE_NAME = 'rozgarsathi-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/home.html',
+  '/profile.html',
+  '/post.html',
+  '/find.html',
+  '/line.html',
+  '/language.html',
+  '/privacy.html',
+  '/mypost.html',
+  '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
